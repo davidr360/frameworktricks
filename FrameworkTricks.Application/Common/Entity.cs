ec88af12d82a4199fa131fc51e4c888d8a0280c2
David <david.reed360@hotmail.co.uk>
2020-06-13 11:31:44 +0100

    Added Entity base class

---
+++ FrameworkTricks.Application/Common/Entity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkTricks.Application.Common
+{
+    public abstract class Entity
+    {
+        public virtual long Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetRealType() != other.GetRealType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetRealType().ToString() + Id).GetHashCode();
+        }
+
+        private Type GetRealType()
+        {
+            return GetRealType();
+        }
+    }
+}
