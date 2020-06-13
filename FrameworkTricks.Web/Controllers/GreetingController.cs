4b8aea0e07c18821c66c4b00ea63ba79de67a710
David <david.reed360@hotmail.co.uk>
2020-06-13 14:00:05 +0100

    Added a greeting from the web config

---
+++ FrameworkTricks.Web/Controllers/GreetingController.cs
@@ -0,0 +1,19 @@
+﻿using FrameworkTricks.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrameworkTricks.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel { Message = ConfigurationManager.AppSettings["message"] };
+            return View(model);
+        }
+    }
+}
