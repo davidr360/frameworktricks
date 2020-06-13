1b0397d8e7e51732ad348843ca88b890f5fd4f87
David <david.reed360@hotmail.co.uk>
2020-06-13 12:41:03 +0100

    Upgraded dependencies and rejigged the content for Bootstrap 4

--- FrameworkTricks.Web/App_Start/BundleConfig.cs
+++ FrameworkTricks.Web/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace FrameworkTricks.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
