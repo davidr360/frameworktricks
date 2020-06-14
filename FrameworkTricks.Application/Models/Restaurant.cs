ea0243f246adbc010b21008a38bc070e52ed3665
David <david.reed360@hotmail.co.uk>
2020-06-14 12:47:49 +0100

    Added server side validations via data annotations

--- FrameworkTricks.Application/Models/Restaurant.cs
+++ FrameworkTricks.Application/Models/Restaurant.cs
@@ -1,13 +1,18 @@
 ﻿using FrameworkTricks.Application.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace FrameworkTricks.Application.Models
 {
     public class Restaurant : Entity
     {
+        [Required]
         public string Name { get; set; }
+
+        [DisplayName("Type of Food")]
         public CuisineType Cuisine { get; set; }
     }
 }
