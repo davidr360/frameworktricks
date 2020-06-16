a69319f99e7658ca31954ec343339afb39678531
David <david.reed360@hotmail.co.uk>
2020-06-16 16:18:15 +0100

    Moved from .NET Standard to .NET Framework for application library in order to satisfy EF6

---
+++ FrameworkTricks.Application/Services/FrameworkTricksDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace FrameworkTricks.Application.Services
+{
+    public class FrameworkTricksDbContext : DbContext
+    {
+    }
+}
